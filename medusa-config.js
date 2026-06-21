63a2022ddb572eef7c914d8557f60e2a3bf398de
usman-faisal <usman.shamsi987@gmail.com>
2026-06-21 16:07:51 +0500

    medusa-config remove bloat

--- medusa-config.js
+++ medusa-config.js
@@ -21,21 +21,21 @@ module.exports = defineConfig({
     },
   },
   modules: [
-    {
-      resolve: '@medusajs/medusa/payment',
-      options: {
-        providers: [
-          {
-            id: 'stripe',
-            resolve: '@medusajs/medusa/payment-stripe',
-            options: {
-              apiKey: process.env.STRIPE_API_KEY,
-              webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
-            },
-          },
-        ],
-      },
-    },
+    // {
+      // resolve: '@medusajs/medusa/payment',
+      // options: {
+      //   providers: [
+      //     {
+      //       id: 'stripe',
+      //       resolve: '@medusajs/medusa/payment-stripe',
+      //       options: {
+      //         apiKey: process.env.STRIPE_API_KEY,
+      //         webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+      //       },
+      //     },
+      //   ],
+      // },
+    // },
     {
       resolve: './src/modules/fashion',
     },
@@ -62,38 +62,38 @@ module.exports = defineConfig({
         ],
       },
     },
-    {
-      resolve: '@medusajs/medusa/notification',
-      options: {
-        providers: [
-          {
-            resolve: './src/modules/resend',
-            id: 'resend',
-            options: {
-              channels: ['email'],
-              api_key: process.env.RESEND_API_KEY,
-              from: process.env.RESEND_FROM,
-              siteTitle: 'SofaSocietyCo.',
-              companyName: 'Sofa Society',
-              footerLinks: [
-                {
-                  url: 'https://agilo.com',
-                  label: 'Agilo',
-                },
-                {
-                  url: 'https://www.instagram.com/agiloltd/',
-                  label: 'Instagram',
-                },
-                {
-                  url: 'https://www.linkedin.com/company/agilo/',
-                  label: 'LinkedIn',
-                },
-              ],
-            },
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: '@medusajs/medusa/notification',
+    //   options: {
+    //     providers: [
+    //       {
+    //         resolve: './src/modules/resend',
+    //         id: 'resend',
+    //         options: {
+    //           channels: ['email'],
+    //           api_key: process.env.RESEND_API_KEY,
+    //           from: process.env.RESEND_FROM,
+    //           siteTitle: 'SofaSocietyCo.',
+    //           companyName: 'Sofa Society',
+    //           footerLinks: [
+    //             {
+    //               url: 'https://agilo.com',
+    //               label: 'Agilo',
+    //             },
+    //             {
+    //               url: 'https://www.instagram.com/agiloltd/',
+    //               label: 'Instagram',
+    //             },
+    //             {
+    //               url: 'https://www.linkedin.com/company/agilo/',
+    //               label: 'LinkedIn',
+    //             },
+    //           ],
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
     {
       resolve: '@medusajs/medusa/event-bus-redis',
       options: {
@@ -138,85 +138,85 @@ module.exports = defineConfig({
         ],
       },
     },
-    {
-      resolve: './src/modules/meilisearch',
-      /**
-       * @type {import('./src/modules/meilisearch/types').MeiliSearchPluginOptions}
-       */
-      options: {
-        config: {
-          host:
-            process.env.MEILISEARCH_HOST ??
-            'https://fashion-starter-search.agilo.agency',
-          apiKey: process.env.MEILISEARCH_API_KEY,
-        },
-        settings: {
-          products: {
-            indexSettings: {
-              searchableAttributes: [
-                'title',
-                'subtitle',
-                'description',
-                'collection',
-                'categories',
-                'type',
-                'tags',
-                'variants',
-                'sku',
-              ],
-              displayedAttributes: [
-                'id',
-                'title',
-                'handle',
-                'subtitle',
-                'description',
-                'is_giftcard',
-                'status',
-                'thumbnail',
-                'collection',
-                'collection_handle',
-                'categories',
-                'categories_handle',
-                'type',
-                'tags',
-                'variants',
-                'sku',
-              ],
-            },
-            primaryKey: 'id',
-            /**
-             * @param {import('@medusajs/types').ProductDTO} product
-             */
-            transformer: (product) => {
-              return {
-                id: product.id,
-                title: product.title,
-                handle: product.handle,
-                subtitle: product.subtitle,
-                description: product.description,
-                is_giftcard: product.is_giftcard,
-                status: product.status,
-                thumbnail: product.images?.[0]?.url ?? null,
-                collection: product.collection.title,
-                collection_handle: product.collection.handle,
-                categories:
-                  product.categories?.map((category) => category.name) ?? [],
-                categories_handle:
-                  product.categories?.map((category) => category.handle) ?? [],
-                type: product.type?.value,
-                tags: product.tags.map((tag) => tag.value),
-                variants: product.variants.map((variant) => variant.title),
-                sku: product.variants
-                  .filter(
-                    (variant) => typeof variant.sku === 'string' && variant.sku,
-                  )
-                  .map((variant) => variant.sku),
-              };
-            },
-          },
-        },
-      },
-    },
+    // {
+    //   resolve: './src/modules/meilisearch',
+    //   /**
+    //    * @type {import('./src/modules/meilisearch/types').MeiliSearchPluginOptions}
+    //    */
+    //   options: {
+    //     config: {
+    //       host:
+    //         process.env.MEILISEARCH_HOST ??
+    //         'https://fashion-starter-search.agilo.agency',
+    //       apiKey: process.env.MEILISEARCH_API_KEY,
+    //     },
+    //     settings: {
+    //       products: {
+    //         indexSettings: {
+    //           searchableAttributes: [
+    //             'title',
+    //             'subtitle',
+    //             'description',
+    //             'collection',
+    //             'categories',
+    //             'type',
+    //             'tags',
+    //             'variants',
+    //             'sku',
+    //           ],
+    //           displayedAttributes: [
+    //             'id',
+    //             'title',
+    //             'handle',
+    //             'subtitle',
+    //             'description',
+    //             'is_giftcard',
+    //             'status',
+    //             'thumbnail',
+    //             'collection',
+    //             'collection_handle',
+    //             'categories',
+    //             'categories_handle',
+    //             'type',
+    //             'tags',
+    //             'variants',
+    //             'sku',
+    //           ],
+    //         },
+    //         primaryKey: 'id',
+    //         /**
+    //          * @param {import('@medusajs/types').ProductDTO} product
+    //          */
+    //         transformer: (product) => {
+    //           return {
+    //             id: product.id,
+    //             title: product.title,
+    //             handle: product.handle,
+    //             subtitle: product.subtitle,
+    //             description: product.description,
+    //             is_giftcard: product.is_giftcard,
+    //             status: product.status,
+    //             thumbnail: product.images?.[0]?.url ?? null,
+    //             collection: product.collection.title,
+    //             collection_handle: product.collection.handle,
+    //             categories:
+    //               product.categories?.map((category) => category.name) ?? [],
+    //             categories_handle:
+    //               product.categories?.map((category) => category.handle) ?? [],
+    //             type: product.type?.value,
+    //             tags: product.tags.map((tag) => tag.value),
+    //             variants: product.variants.map((variant) => variant.title),
+    //             sku: product.variants
+    //               .filter(
+    //                 (variant) => typeof variant.sku === 'string' && variant.sku,
+    //               )
+    //               .map((variant) => variant.sku),
+    //           };
+    //         },
+    //       },
+    //     },
+    //   },
+    // },
   ],
   plugins: [
     {
